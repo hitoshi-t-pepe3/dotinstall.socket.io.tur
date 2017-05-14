97ab0706a4d66eec2079be3951b802f114033e0d
pepechoko <hitoshi.t.pepe3@gmail.com>
2017-05-15 00:32:06 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs'); 
+app.listen(1337);
+// io.set("log level",1);
+function handler(res, res) {
+  fs.readFile(__dirname + '/index.html', function(err, data){
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error');
+    }
+    res.writeHead(200);
+    res.write(data);
+    res.end();
+  })
+  io.sockets.on('connection', function(socket){
+    socket.on('emit_from_client', function(data){
+      console.log(data);
+    })
+  })
+}
